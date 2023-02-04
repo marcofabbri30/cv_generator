96082a6aeb79fa6c3fd430a3dabf6493089fe051
mfabbri <marco.fabbri30@gmail.com>
2023-02-04 08:06:39 +0100

    aggiunto data e altri campi

---
+++ app/data.py
@@ -0,0 +1,47 @@
+data = {
+    'esperienze_professionali': [
+        {
+            'posizione':'poltronesofà - Software Engineer',
+            'periodo':'03/2021 &rarr; presente',
+            'luogo':'Forlì (FC) - Emilia-Romagna ',
+            'esperienza':[
+                'Analista e sviluppatore software per il miglioramento dei processi di business. Sviluppo di microservizi per l\'integrazione con i terzisti, i negozi, i servizi di logistica, gestione e manutenzione del sito aziendale e dei servizi web.',
+                'Tecnologie utilizzate: FastAPI, Python, .Net Framework, Vue.js, database Oracle e Sql Server, Docker, sistema di versionamento Git e Gitlab, Flask, Apache Airflow, Linux',
+                'Sviluppo e manutenzione della Business Intelligence in Qlik Sense.'
+            ]
+        }
+    ],
+    'formazione': [
+        {
+            'titolo': 'Laurea Magistrale in Ingegneria Biomedica (110/110)',
+            'anno': 2017,
+            'sede':  'Università di Bologna - Sede di Cesena'
+        },
+        {
+            'titolo': 'Laurea triennale in Ingegneria Biomedica (110/110)',
+            'anno': 2013,
+            'sede':  'Università di Bologna - Sede di Cesena'
+        },
+        {
+            'titolo': 'Diploma di liceo scientifico - indirizzo bilinguismo inglese/tedesco',
+            'anno': 2010,
+            'sede':  'Liceo Scientifico A. Einstein - Rimini (RN)'
+        }
+    ],
+    'competenze_informatiche':{
+        'linguaggi': ['Python', 'C#', 'SQL', 'Javascript'],
+        'tecnologie': ['FastAPI', 'Vue3', 'Flask', 'Qlik Sense'],
+    },
+    'competenze_linguistiche': [
+        {
+            'lingua': 'Inglese',
+            'voto': 'Ottima',
+            'altro': 'Attestato Idoneità B2'
+        },
+        {
+            'lingua': 'Tedesco',
+            'voto': 'Base',
+            'altro': 'Diploma di liceo'
+        }
+    ]
+}
